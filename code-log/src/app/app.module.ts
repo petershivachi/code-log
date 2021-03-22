393d78215290874c4d3cc8f354d1fd542186aa02
petershivachi <shitsukanepeter@gmailc.om>
2021-03-22 22:09:56 +0300

    created the ui

--- code-log/src/app/app.module.ts
+++ code-log/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CodeLogComponent } from './components/code-log/code-log.component';
+import { LogFormComponent } from './components/log-form/log-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CodeLogComponent,
+    LogFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ code-log/src/app/components/code-log/code-log.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-log',
+  templateUrl: './code-log.component.html',
+  styleUrls: ['./code-log.component.css']
+})
+export class CodeLogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
