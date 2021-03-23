f090ad3f7c55b4883d356b0517d7ac0fd584b396
petershivachi <shitsukanepeter@gmailc.om>
2021-03-23 13:47:31 +0300

    finished up the state UX

--- code-log/src/app/components/code-log/code-log.component.ts
+++ code-log/src/app/components/code-log/code-log.component.ts
@@ -9,23 +9,32 @@ import { LogService } from '../../services/log.service';
 })
 export class CodeLogComponent implements OnInit {
   logs: Log[];
+  selectedLog: Log;
+  loaded: boolean = false;
 
   constructor( private logService: LogService) { }
 
   ngOnInit(): void {
-    this.logService.getLogs().subscribe(logs => this.logs = logs);
+    this.logService.stateClear.subscribe(clear => {
+      if(clear){
+        this.selectedLog = { id: '', text: '', date: ''};
+      }
+    })
+    this.logService.getLogs().subscribe(logs => {
+      this.logs = logs;
+      this.loaded = true;
+    });
    }
 
    onSelect(log: Log){
      this.logService.setFormLog(log);
+     this.selectedLog = log;
    }
 
    onDelete(log: Log){
-     this.logs.forEach((cur, index) => {
-       if(log.id === cur.id){
-         this.logs.splice(index, 1);
-       }
-     })
+     if(confirm('Are you sure?')){
+       this.logService.deleteLog(log)
+     }
    }
 
 }
