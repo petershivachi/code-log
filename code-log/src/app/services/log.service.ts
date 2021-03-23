286021af4406019dd0770ff9fbbe899f521c6a68
petershivachi <shitsukanepeter@gmailc.om>
2021-03-23 14:04:50 +0300

    implemented local storage

--- code-log/src/app/services/log.service.ts
+++ code-log/src/app/services/log.service.ts
@@ -28,7 +28,14 @@ export class LogService {
    }
 
    getLogs(): Observable<Log[]>{
-     return of(this.logs)
+     if(localStorage.getItem('logs') === null){
+       this.logs = []
+     }else {
+       this.logs = JSON.parse(localStorage.getItem('logs'));
+     }
+     return of(this.logs.sort((a, b) => 
+       b.date - a.date
+     ))
    }
 
    setFormLog(log: Log){
@@ -37,6 +44,8 @@ export class LogService {
 
    addLog(log: Log){
      this.logs.unshift(log)
+
+     localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    updateLog(log: Log){
@@ -46,6 +55,8 @@ export class LogService {
        }
      });
      this.logs.unshift(log)
+
+     localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    deleteLog(log: Log){
@@ -54,6 +65,8 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     })
+
+    localStorage.setItem('logs', JSON.stringify(this.logs));
    }
 
    clearState(){
